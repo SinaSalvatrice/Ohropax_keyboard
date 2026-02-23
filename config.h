fc0887818981722081b1f847288399e5defffbc6
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-02-23 15:59:23 +0100

    Add configuration settings in config.h

---
+++ config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define DEBOUNCE 25
+
+// RGB / WS2812
+#define RGBLIGHT_LIMIT_VAL 150
+#define RGBLIGHT_SLEEP
+
+// Encoder: Viele Encoder sind 4 Schritte pro Rastung.
+// Das macht’s „normal“ statt Maschinengewehr.
+#define ENCODER_RESOLUTION 4
