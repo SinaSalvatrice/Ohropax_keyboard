dfd739af671c934a1b661608f375c2c2f827dc31
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-02-23 16:02:30 +0100

    Add configuration options to rules.mk

---
+++ rules.mk
@@ -0,0 +1,11 @@
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+
+ENCODER_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+
+RGBLIGHT_ENABLE = yes
+WS2812_DRIVER = vendor
+
+# optional, falls du später Makros brauchst:
+# TAP_DANCE_ENABLE = yes
